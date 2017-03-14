4bfd53eaa6b6842131fa219204687c1ba1037504
yipbaozaifan <yepeizhan@163.com>
2017-03-14 08:52:07 +0800

    修改数据库模型

--- model/models.js
+++ model/models.js
@@ -1,52 +1,36 @@
 var mongoose = require("mongoose");
 
-var db = mongoose.createConnection('mongodb://localhost/demo');
+var db = mongoose.createConnection('mongodb://localhost/survey');
 //登录注册
 var UserSchema = new mongoose.Schema({
 	username:String,
 	password:String,
-	nicName:String,
-	url:String,
-	role:String,
-	done_survey:[mongoose.Schema.Types.ObjectId]
+	nicName:String
 })
 
 //问卷数据库
 var survey = new mongoose.Schema({
-	user:mongoose.Schema.Types.ObjectId,
-	begin:mongoose.Schema.Types.ObjectId,
-	end:mongoose.Schema.Types.ObjectId,
-	background:String,
+	user:{type:mongoose.Schema.Types.ObjectId},
 	survey_name:String,
 	count:Number,
-	questions:[mongoose.Schema.Types.ObjectId],
-	tab:String
+	intro:String,
+	end:String,
+	status:Number,
+	question:Number
 })
 //问卷中每条问题的数据库（选项型问题）
 var questions = new mongoose.Schema({
-	survey:mongoose.Schema.Types.ObjectId,
-	order:Number,
-	describe:String,
+	survey:String,
+	title:String,
+	desc:String,
 	type:String,
-	options:[mongoose.Schema.Types.ObjectId]
-})
-//问题数据库（问答型问题）
-var text_questions = new mongoose.Schema({
-	survey:mongoose.Schema.Types.ObjectId,
-	order:Number,
-	describe:String,
-	type:String,
-	answer:String
-})
-var options = new mongoose.Schema({
-	question:mongoose.Schema.Types.ObjectId,
-	order:Number,
-	content:String
+	index:Number,
+	options:[]
 })
 //各问题回答
 var answers = new mongoose.Schema({
 	survey:mongoose.Schema.Types.ObjectId,
-	questions:mongoose.Schema.Types.ObjectId,
+	questions:String,
 	choices:String
 })
 
@@ -54,4 +38,10 @@ db.on('error', function(error) {
   console.log(error);
 });
 
-exports.db = db;
+var userModel = db.model('user',UserSchema,'user');
+var surveyModel = db.model('survey',survey,'survey');
+var questionModel = db.model('question',questions,'question');
+
+exports.User = userModel;
+exports.Survey = surveyModel;
+exports.Question = questionModel;
